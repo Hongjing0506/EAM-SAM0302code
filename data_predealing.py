8b4b8ceefc2feffb90b2957b1851e5543b07ad5d
Hongjing0506 <18520228343@163.com>
2022-03-02 17:10:40 +0800

    CMIP6.py

--- data_predealing.py
+++ data_predealing.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-03-02 16:45:05
 LastEditors: ChenHJ
-LastEditTime: 2022-03-02 16:45:06
+LastEditTime: 2022-03-02 17:10:17
 FilePath: /chenhj/0302code/data_predealing.py
 Aim: 
 Mission: 
@@ -39,3 +39,13 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from scipy.stats import t
 # %%
+modelname = ["ACCESS-CM2", "BCC-CSM2-MR", "CAMS-CSM1-0", "CanESM5", "CESM2", "CESM2-WACCM", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3", "FGOALS-g3", "GFDL-CM4", "HadGEM3-GC31-LL", "IITM-ESM", "INM-CM4-8", "INM-CM5-0", "IPSL-CM6A-LR", "KACE-1-0-G", "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MRI-ESM2-0", "NESM3", "NorESM2-LM", "TaiESM1", "UKESM1-0-LL"]
+rlzn = ["r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r11i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f2", "r1i1p1f2", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f3", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f2", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r3i1p1f2"]
+for model,rl in zip(modelname, rlzn):
+    print(model)
+    srcPath = "/home/ys17-23/Extension/CMIP6/CMIP6/"+ model +"/historical/" + rl + "/Amon"
+    tmpPath = "/home/ys17-23/chenhj/CMIP6/tmpPath"
+    dstPath = "/home/ys17-23/chenhj/CMIP6/historical"
+    variable = ["zg", "ta", "wap", "ua", "va", "pr", "hus"]
+    freq = "Amon"
+    bash_merge_process(srcPath, tmpPath, dstPath, variable, freq, rl)
