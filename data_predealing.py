9abcf208c6d22444f68e4eca257037a209ce1b98
Hongjing0506 <18520228343@163.com>
2022-03-02 16:45:50 +0800

    data_predealing.py

---
+++ data_predealing.py
@@ -0,0 +1,41 @@
+'''
+Author: ChenHJ
+Date: 2022-03-02 16:45:05
+LastEditors: ChenHJ
+LastEditTime: 2022-03-02 16:45:06
+FilePath: /chenhj/0302code/data_predealing.py
+Aim: 
+Mission: 
+'''
+# %%
+import numpy as np
+import xarray as xr
+import os
+import re
+from cdo import Cdo
+import shutil
+import sys
+
+sys.path.append("/home/ys17-23/chenhj/self_def/")
+import plot as sepl
+import cal as ca
+import pandas as pd
+from importlib import reload
+
+# sd.path.append("/home/ys17-23/chenhj/1201code/self_def.py")
+
+cdo = Cdo()
+
+# for plot
+import proplot as pplt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.ticker import LongitudeFormatter
+from cartopy.mpl.ticker import LatitudeFormatter
+from cartopy.util import add_cyclic_point
+from matplotlib.ticker import MultipleLocator
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.stats import t
+# %%
