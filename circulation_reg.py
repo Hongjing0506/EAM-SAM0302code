65f3bf91985e9ea4c00acd346ea044b860f6a475
Hongjing0506 <18520228343@163.com>
2022-03-16 17:42:27 +0800

    circulation_reg

---
+++ circulation_reg.py
@@ -0,0 +1,44 @@
+'''
+Author: ChenHJ
+Date: 2022-03-16 17:42:02
+LastEditors: ChenHJ
+LastEditTime: 2022-03-16 17:42:03
+FilePath: /chenhj/0302code/circulation_reg.py
+Aim: 
+Mission: 
+'''
+# %%
+import numpy as np
+import xarray as xr
+import os
+import re
+from cdo import Cdo
+import shutil
+import sys
+
+sys.path.append("/home/ys17-23/chenhj/self_def/")
+import plot as sepl
+import cal as ca
+import pandas as pd
+from importlib import reload
+
+reload(sepl)
+
+# sd.path.append("/home/ys17-23/chenhj/1201code/self_def.py")
+
+cdo = Cdo()
+
+# for plot
+import proplot as pplt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.ticker import LongitudeFormatter
+from cartopy.mpl.ticker import LatitudeFormatter
+from cartopy.util import add_cyclic_point
+from matplotlib.ticker import MultipleLocator
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.stats import t
+from eofs.multivariate.standard import MultivariateEof
+from eofs.standard import Eof
