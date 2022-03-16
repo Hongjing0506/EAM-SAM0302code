d53642c53b5d511452c21881ed31ac653575104c
Hongjing0506 <18520228343@163.com>
2022-03-16 12:11:34 +0800

    _9

--- precip_reg.py
+++ precip_reg.py
@@ -678,32 +678,7 @@ sepl.patches(axs[0,2], 108, 36, 10.0, 6.0, proj)
 fig_std.colorbar(con, loc="b", width=0.13, length=0.7)
 fig_std.format(abc="(a)", abcloc="l")
 # %%
-
-axs[2].line(
-    his_India_EA_regress_7.time.dt.year,
-    np.array(his_India_EA_regress_7["rvalue"]),
-    lw=lw,
-    color="black",
-)
-axs[2].format(title = "historical")
-
-for i, ax in enumerate(axs[3:]):
-    ax.line(his_ds_India_EA_rvalue_7.time.dt.year, his_ds_India_EA_rvalue_7[:,i], color="black", lw=lw)
-    ax.format(title=np.array(his_ds_India_EA_rvalue_7.coords["models"][i]))
-
-for ax in axs:
-    ax.axhline(0, lw = 0.8, color="grey5", linestyle="--")
-    ax.axhline(0.6664, lw = 0.8, color="grey5", linestyle="--")
-    ax.axhline(-0.6664, lw = 0.8, color="grey5", linestyle="--")
-    ax.axvline(1972, lw=0.8, color="grey5", linestyle="--")
-    ax.format(ltitle="window=7", rtitle="1950-2014")
-
-axs.format(
-    ylim=(-1.0, 1.0), ylocator=0.2, yminorlocator=0.1, xrotation=0, xlim=(1950, 2014),
-)
-fig.format(abc="(a)", abcloc="l")
-# %%
-
+#   plot the rolling_reg_index
 fig = pplt.figure(refwidth=5.0, refheight=2.5, span=False, share=False)
 axs = fig.subplots(ncols=5, nrows=6)
 lw = 0.8
@@ -744,6 +719,53 @@ for ax in axs:
     ax.axhline(-0.6664, lw = 0.8, color="grey5", linestyle="--")
     ax.axvline(1972, lw=0.8, color="grey5", linestyle="--")
     ax.format(ltitle="window=7", rtitle="1950-2014")
+
+axs.format(
+    ylim=(-1.0, 1.0), ylocator=0.2, yminorlocator=0.1, xrotation=0, xlim=(1950, 2014),
+)
+fig.format(abc="(a)", abcloc="l")
+# %%
+
+fig = pplt.figure(refwidth=5.0, refheight=2.5, span=False, share=False)
+axs = fig.subplots(ncols=5, nrows=6)
+lw = 0.8
+# cycle = pplt.Cycle('Pastel1', 'Pastel2', 27, left=0.1)
+cycle = "Pastel1"
+
+axs[0].line(
+    CRU_India_EA_regress_9.time.dt.year,
+    np.array(CRU_India_EA_regress_9["rvalue"]),
+    lw=lw,
+    color="black",
+)
+axs[0].format(title = "CRU TS4.01")
+
+axs[1].line(
+    GPCP_India_EA_regress_9.time.dt.year,
+    np.array(GPCP_India_EA_regress_9["rvalue"]),
+    lw=lw,
+    color="black",
+)
+axs[1].format(title = "GPCP")
+
+axs[2].line(
+    his_India_EA_regress_9.time.dt.year,
+    np.array(his_India_EA_regress_9["rvalue"]),
+    lw=lw,
+    color="black",
+)
+axs[2].format(title = "historical")
+
+for i, ax in enumerate(axs[3:]):
+    ax.line(his_ds_India_EA_rvalue_9.time.dt.year, his_ds_India_EA_rvalue_9[:,i], color="black", lw=lw)
+    ax.format(title=np.array(his_ds_India_EA_rvalue_9.coords["models"][i]))
+
+for ax in axs:
+    ax.axhline(0, lw = 0.8, color="grey5", linestyle="--")
+    ax.axhline(0.6021, lw = 0.8, color="grey5", linestyle="--")
+    ax.axhline(-0.6021, lw = 0.8, color="grey5", linestyle="--")
+    ax.axvline(1972, lw=0.8, color="grey5", linestyle="--")
+    ax.format(ltitle="window=9", rtitle="1950-2014")
 
 axs.format(
     ylim=(-1.0, 1.0), ylocator=0.2, yminorlocator=0.1, xrotation=0, xlim=(1950, 2014),
