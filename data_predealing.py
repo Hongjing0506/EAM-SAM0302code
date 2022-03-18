2531399031db791ebd98204f1c773686138f01ae
Hongjing0506 <18520228343@163.com>
2022-03-18 16:42:27 +0800

    cdo_ensmean

--- data_predealing.py
+++ data_predealing.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-03-02 16:45:05
 LastEditors: ChenHJ
-LastEditTime: 2022-03-18 15:25:06
+LastEditTime: 2022-03-18 16:42:17
 FilePath: /chenhj/0302code/data_predealing.py
 Aim: 
 Mission: 
@@ -145,12 +145,25 @@ for var in variable:
             uniform_plev(os.path.join(path, filename), os.path.join("/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/ssp5852", var, filename), var)
 # %%
 #   calculate the ensmean of different variables
-srcPath = ""
-dstPath = ""
+    
+def cdo_ensmean(srcPath, dstPath):
+    g = os.walk(srcPath)
+    inputString = ""
+    mergelist = []
+    for path, dir_list, file_list in g:
+        for file_name in file_list:
+            if re.search("IITM-ESM", file_name) == None and re.search("ensemble", file_name) == None:
+                mergelist.append(os.path.join(path, file_name))
+    for i in range(len(mergelist)):
+        inputString += mergelist[i] + " "
+    print(inputString)
+    cdo.ensmean(input=inputString, output=os.path.join(dstPath, var + "_Amon_ensemble_ssp585_gn_201501-209912.nc"))
+    
 variables = ["ua", "va", "hus", "ta", "wap", "ps", "pr", "zg"]
 for var in variables:
-    def cdo_ensmean(srcPath, dstPath):
-
+    srcPath = os.path.join("/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/ssp585", var)
+    dstPath = os.path.join("/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/ssp585", var)
+    cdo_ensmean(srcPath, dstPath)
 # %%
 modelname = ["ACCESS-CM2", "BCC-CSM2-MR", "CAMS-CSM1-0", "CanESM5", "CESM2", "CESM2-WACCM", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3", "FGOALS-g3", "GFDL-CM4", "HadGEM3-GC31-LL", "INM-CM4-8", "INM-CM5-0", "IPSL-CM6A-LR", "KACE-1-0-G", "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MRI-ESM2-0", "NESM3", "NorESM2-LM", "TaiESM1", "UKESM1-0-LL"]
 rlzn = ["r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r11i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f2", "r1i1p1f2", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f3", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f2", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r3i1p1f2"]
