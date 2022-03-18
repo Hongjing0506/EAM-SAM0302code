d6dbeb25cba4fba2ad94d643e9f99ed4184dac0f
Hongjing0506 <18520228343@163.com>
2022-03-18 15:06:12 +0800

    predealing

--- data_predealing.py
+++ data_predealing.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-03-02 16:45:05
 LastEditors: ChenHJ
-LastEditTime: 2022-03-12 16:36:47
+LastEditTime: 2022-03-18 15:04:46
 FilePath: /chenhj/0302code/data_predealing.py
 Aim: 
 Mission: 
@@ -143,4 +143,31 @@ for var in variable:
         for filename in file_list:
             print(filename)
             uniform_plev(os.path.join(path, filename), os.path.join("/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/ssp5852", var, filename), var)
+# %%
+#   calculate the ensmean of different variables
+srcPath = ""
+dstPath = ""
+var = ["ua", "va", "hus", "ta", "wap", ]
+
+
+# %%
+modelname = ["ACCESS-CM2", "BCC-CSM2-MR", "CAMS-CSM1-0", "CanESM5", "CESM2", "CESM2-WACCM", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3", "FGOALS-g3", "GFDL-CM4", "HadGEM3-GC31-LL", "INM-CM4-8", "INM-CM5-0", "IPSL-CM6A-LR", "KACE-1-0-G", "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MRI-ESM2-0", "NESM3", "NorESM2-LM", "TaiESM1", "UKESM1-0-LL"]
+rlzn = ["r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r11i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f2", "r1i1p1f2", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f3", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f2", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r1i1p1f1", "r3i1p1f2"]
+for model,rl in zip(modelname, rlzn):
+    print(model)
+    srcPath = "/home/ys17-23/Extension/CMIP6/CMIP6/"+ model +"/historical/" + rl + "/Amon"
+    tmpPath = "/home/ys17-23/chenhj/CMIP6/tmpPath"
+    dstPath = "/home/ys17-23/chenhj/CMIP6/historical"
+    variable = ["ps"]
+    freq = "Amon"
+    ca.CMIP6_predealing_1(srcPath, tmpPath, dstPath, variable, freq, rl)
+# %%
+#   select time
+variable = ["ps"]
+for var in variable:
+    srcPath = "/home/ys17-23/chenhj/CMIP6/historical/" + var
+    dstPath = "/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/historical/" + var
+    start = 1950
+    end = 2014
+    ca.p_year(srcPath, dstPath, start, end)
 # %%
