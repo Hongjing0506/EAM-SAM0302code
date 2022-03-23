7edf71dddb2e25314176282447a99ac52d8d666f
Hongjing0506 <18520228343@163.com>
2022-03-23 18:28:20 +0800

    path

--- circulation_reg.py
+++ circulation_reg.py
@@ -77,61 +77,61 @@ prehis_ds = ca.detrend_dim(prehis_ds, "time", deg=1, demean=False)
 prehis_ds.coords["models"] = modelname_pr
 
 fvERA5 = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/obs/vwind_mon_r144x72_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/obs/vwind_mon_r144x72_195001-201412.nc"
 )
 vERA5 = fvERA5["v"]
 vERA5 = ca.detrend_dim(vERA5, "time", deg=1, demean=False)
 
 fvhis = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/historical/va/va_Amon_ensemble_historical_gn_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/CMIP6/historical/va/va_Amon_ensemble_historical_gn_195001-201412.nc"
 )
 vhis = fvhis["va"]
 vhis = ca.detrend_dim(vhis, "time", deg=1, demean=False)
 
 fuERA5 = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/obs/uwind_mon_r144x72_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/obs/uwind_mon_r144x72_195001-201412.nc"
 )
 uERA5 = fuERA5["u"]
 uERA5 = ca.detrend_dim(uERA5, "time", deg=1, demean=False)
 
 fuhis = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/historical/ua/ua_Amon_ensemble_historical_gn_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/CMIP6/historical/ua/ua_Amon_ensemble_historical_gn_195001-201412.nc"
 )
 uhis = fuhis["ua"]
 uhis = ca.detrend_dim(uhis, "time", deg=1, demean=False)
 
 fspERA5 = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/obs/sp_mon_r144x72_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/obs/sp_mon_r144x72_195001-201412.nc"
 )
 spERA5 = fspERA5["sp"]
 spERA5 = ca.detrend_dim(spERA5, "time", deg=1, demean=False)
 
 fsphis = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/historical/ps/ps_Amon_ensemble_historical_gn_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/CMIP6/historical/ps/ps_Amon_ensemble_historical_gn_195001-201412.nc"
 )
 sphis = fsphis["ps"]
 sphis = ca.detrend_dim(sphis, "time", deg=1, demean=False)
 
 fqERA5 = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/obs/q_mon_r144x72_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/obs/q_mon_r144x72_195001-201412.nc"
 )
 qERA5 = fqERA5["q"]
 qERA5 = ca.detrend_dim(qERA5, "time", deg=1, demean=False)
 
 fqhis = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/historical/hus/hus_Amon_ensemble_historical_gn_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/CMIP6/historical/hus/hus_Amon_ensemble_historical_gn_195001-201412.nc"
 )
 qhis = fqhis["hus"]
 qhis = ca.detrend_dim(qhis, "time", deg=1, demean=False)
 
 fhgtERA5 = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/obs/hgt_mon_r144x72_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/obs/hgt_mon_r144x72_195001-201412.nc"
 )
 hgtERA5 = fhgtERA5["z"]
 hgtERA5 = ca.detrend_dim(hgtERA5, "time", deg=1, demean=False)
 
 fhgthis = xr.open_dataset(
-    "/home/ys17-23/chenhj/SAM_EAM_data/CMIP6/historical/zg/zg_Amon_ensemble_historical_gn_195001-201412.nc"
+    "/home/ys17-23/Extension/persenal-data/chenhj/SAM_EAM_data/CMIP6/historical/zg/zg_Amon_ensemble_historical_gn_195001-201412.nc"
 )
 hgthis = fhgthis["zg"]
 hgthis = ca.detrend_dim(hgthis, "time", deg=1, demean=False)
