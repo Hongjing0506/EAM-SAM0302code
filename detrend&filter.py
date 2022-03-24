cff640abc7ca8850f2c5138ebb8413bb4be9b1d5
Hongjing0506 <18520228343@163.com>
2022-03-24 22:41:20 +0800

    calculate regress and plot

--- detrend&filter.py
+++ detrend&filter.py
@@ -148,6 +148,202 @@ prehis_India_mean = ca.cal_lat_weighted_mean(prehis_India_JJA).mean(
 prehis_EA_mean = ca.cal_lat_weighted_mean(prehis_EA_JJA).mean(dim="lon", skipna=True)
 
 # %%
+(
+    CRU_India_slope,
+    CRU_India_intercept,
+    CRU_India_rvalue,
+    CRU_India_pvalue,
+    CRU_India_hypothesis,
+) = ca.dim_linregress(preCRU_India_mean, preCRU_JJA)
+
+(
+    CRU_EA_slope,
+    CRU_EA_intercept,
+    CRU_EA_rvalue,
+    CRU_EA_pvalue,
+    CRU_EA_hypothesis,
+) = ca.dim_linregress(preCRU_EA_mean, preCRU_JJA)
+
+(
+    CRU_Japan_slope,
+    CRU_Japan_intercept,
+    CRU_Japan_rvalue,
+    CRU_Japan_pvalue,
+    CRU_Japan_hypothesis,
+) = ca.dim_linregress(preCRU_Japan_mean, preCRU_JJA)
+
+(
+    his_India_slope,
+    his_India_intercept,
+    his_India_rvalue,
+    his_India_pvalue,
+    his_India_hypothesis,
+) = ca.dim_linregress(prehis_India_mean, prehis_JJA)
+
+(
+    his_EA_slope,
+    his_EA_intercept,
+    his_EA_rvalue,
+    his_EA_pvalue,
+    his_EA_hypothesis,
+) = ca.dim_linregress(prehis_EA_mean, prehis_JJA)
+
+(
+    his_Japan_slope,
+    his_Japan_intercept,
+    his_Japan_rvalue,
+    his_Japan_pvalue,
+    his_Japan_hypothesis,
+) = ca.dim_linregress(prehis_Japan_mean, prehis_JJA)
+
+# %%
+#   plot the rvalue distribution for different area precipitation
+pplt.rc.grid = False
+pplt.rc.reso = "lo"
+cl = 0  # 设置地图投影的中心纬度
+proj = pplt.PlateCarree(central_longitude=cl)
+
+fig_rvalue = pplt.figure(
+    span=False, share=False, refwidth=4.0, wspace=4.0, hspace=3.5, outerpad=2.0
+)
+axs = fig_rvalue.subplots(ncols=3, nrows=2, proj=proj)
+
+#   set the geo_ticks and map projection to the plots
+xticks = np.arange(50, 151, 10)  # 设置纬度刻度
+yticks = np.arange(10, 51, 10)  # 设置经度刻度
+# 设置绘图的经纬度范围extents，其中前两个参数为经度的最小值和最大值，后两个数为纬度的最小值和最大值
+# 当想要显示的经纬度范围不是正好等于刻度显示范围时，对extents进行相应的修改即可
+extents = [xticks[0], xticks[-1], 5, 55]
+sepl.geo_ticks(axs, xticks, yticks, cl, 5, 5, extents)
+n = 1
+# ==========================
+con = axs[0, 0].contourf(
+    CRU_India_rvalue, cmap="ColdHot", levels=np.arange(-1.0, 1.1, 0.1),
+)
+sepl.plt_sig(
+    CRU_India_pvalue,
+    axs[0, 0],
+    n,
+    np.where(CRU_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+# axs[0,0].contour(
+#     CRU_India_pvalue,
+#     color="black",
+#     vmin=0.05,
+#     vmax=0.05,
+#     lw=0.8
+# )
+axs[0, 0].format(
+    title="Pr reg IndR", rtitle="1950-2014", ltitle="CRU TS4.01",
+)
+sepl.patches(axs[0, 0], 70.0, 8.0, 16.0, 20.0, proj)
+# ==========================
+axs[1, 0].contourf(
+    CRU_EA_rvalue, cmap="ColdHot", levels=np.arange(-1.0, 1.1, 0.1),
+)
+sepl.plt_sig(
+    CRU_EA_pvalue, axs[1, 0], n, np.where(CRU_EA_pvalue[::n, ::n] <= 0.05), "denim", 3.0
+)
+# axs[0,0].contour(
+#     CRU_India_pvalue,
+#     color="black",
+#     vmin=0.05,
+#     vmax=0.05,
+#     lw=0.8
+# )
+axs[1, 0].format(
+    title="Pr reg NCR", rtitle="1950-2014", ltitle="CRU TS4.01",
+)
+sepl.patches(axs[1, 0], 108, 36, 10.0, 6.0, proj)
+# ==========================
+axs[2, 0].contourf(
+    CRU_Japan_rvalue, cmap="ColdHot", levels=np.arange(-1.0, 1.1, 0.1),
+)
+sepl.plt_sig(
+    CRU_Japan_pvalue,
+    axs[2, 0],
+    n,
+    np.where(CRU_Japan_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+# axs[0,0].contour(
+#     CRU_India_pvalue,
+#     color="black",
+#     vmin=0.05,
+#     vmax=0.05,
+#     lw=0.8
+# )
+axs[2, 0].format(
+    title="Pr reg SJR", rtitle="1950-2014", ltitle="CRU TS4.01",
+)
+sepl.patches(axs[2, 0], 130, 31, 10.0, 5.0, proj)
+# ==========================
+axs[0, 1].contourf(
+    his_India_rvalue, cmap="ColdHot", levels=np.arange(-1.0, 1.1, 0.1),
+)
+# axs[0,2].contour(
+#     his_India_pvalue,
+#     color="black",
+#     levels=np.array([0.01, 0.05])
+# )
+sepl.plt_sig(
+    his_India_pvalue,
+    axs[0, 1],
+    n,
+    np.where(his_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[0, 1].format(
+    title="Pr reg IndR", rtitle="1950-2014", ltitle="historical",
+)
+sepl.patches(axs[0, 1], 70.0, 8.0, 16.0, 20.0, proj)
+# ==========================
+axs[1, 1].contourf(
+    his_EA_rvalue, cmap="ColdHot", levels=np.arange(-1.0, 1.1, 0.1),
+)
+# axs[0,2].contour(
+#     his_India_pvalue,
+#     color="black",
+#     levels=np.array([0.01, 0.05])
+# )
+sepl.plt_sig(
+    his_EA_pvalue, axs[1, 1], n, np.where(his_EA_pvalue[::n, ::n] <= 0.05), "denim", 3.0
+)
+axs[1, 1].format(
+    title="Pr reg NCR", rtitle="1950-2014", ltitle="historical",
+)
+sepl.patches(axs[1, 1], 108, 36, 10.0, 6.0, proj)
+# ==========================
+axs[2, 1].contourf(
+    his_Japan_rvalue, cmap="ColdHot", levels=np.arange(-1.0, 1.1, 0.1),
+)
+# axs[0,2].contour(
+#     his_India_pvalue,
+#     color="black",
+#     levels=np.array([0.01, 0.05])
+# )
+sepl.plt_sig(
+    his_Japan_pvalue,
+    axs[2, 1],
+    n,
+    np.where(his_Japan_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[2, 1].format(
+    title="Pr reg SJR", rtitle="1950-2014", ltitle="historical",
+)
+sepl.patches(axs[2, 1], 130, 31, 10.0, 5.0, proj)
+
+fig_rvalue.colorbar(con, loc="b", width=0.13, length=0.7, label="")
+fig_rvalue.format(abc="(a)", abcloc="l")
+
+
+# %%
 window = 7
 freq = "AS-JUL"
 CRUtime = preCRU_India_mean.coords["time"]
