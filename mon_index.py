8a930b581e7a6689f083d801eb55daf71bb8685a
Hongjing0506 <18520228343@163.com>
2022-03-28 21:08:28 +0800

    plot the windshear and vorticity reg on IndR

--- mon_index.py
+++ mon_index.py
@@ -2504,6 +2504,8 @@ sepl.plt_sig(
     3.0,
 )
 axs[2, 1].format(ltitle="historical", rtitle="vorticity reg IWF")
+fig.colorbar(con, loc="b", width=0.13, length=0.7, label="")
+fig.format(abc="(a)", abcloc="l")
 # %%
 #   calculate the Indian JJA mean rainfall and its water vapor source mean
 preCRU_India_JJA = ca.cal_lat_weighted_mean(preCRU_JJA.loc[:, 8:28, 70:86]).mean(
@@ -2649,4 +2651,193 @@ prehis_India_JJA.coords["time"] = ushearhis_JJA.coords["time"]
     vor_his_uqIndia_pvalue,
     vor_his_uqIndia_hypothesis,
 ) = ca.dim_linregress(uq_dpg_his_India_JJA, vorhis_JJA)
+# %%
+pplt.rc.grid = False
+pplt.rc.reso = "lo"
+cl = 0  # 设置地图投影的中心纬度
+proj = pplt.PlateCarree(central_longitude=cl)
+
+fig = pplt.figure(
+    span=False, share=False, refwidth=4.0, wspace=4.0, hspace=3.5, outerpad=2.0
+)
+axs = fig.subplots(ncols=3, nrows=3, proj=proj)
+
+#   set the geo_ticks and map projection to the plots
+xticks = np.array([30, 60, 90, 120, 150, 180])  # 设置纬度刻度
+yticks = np.arange(-30, 46, 15)  # 设置经度刻度
+# 设置绘图的经纬度范围extents，其中前两个参数为经度的最小值和最大值，后两个数为纬度的最小值和最大值
+# 当想要显示的经纬度范围不是正好等于刻度显示范围时，对extents进行相应的修改即可
+extents = [xticks[0], xticks[-1], yticks[0], 55.0]
+sepl.geo_ticks(axs, xticks, yticks, cl, 10, 5, extents)
+
+# ===================================================
+ski = 2
+n = 1
+w, h = 0.12, 0.14
+# ===================================================
+for ax in axs:
+    x0 = 90.0
+    y0 = 5.0
+    width = 50.0
+    height = 27.5
+    patches(ax, x0 - cl, y0, width, height, proj)
+# ===================================================
+con = axs[0, 0].contourf(
+    ushear_CRU_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    ushear_CRU_India_pvalue,
+    axs[0, 0],
+    n,
+    np.where(ushear_CRU_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[0, 0].format(ltitle="CRU TS4.01 & ERA5", rtitle="U850-U200 reg IndR")
+# ===================================================
+con = axs[1, 0].contourf(
+    vshear_CRU_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    vshear_CRU_India_pvalue,
+    axs[1, 0],
+    n,
+    np.where(vshear_CRU_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[1, 0].format(ltitle="CRU TS4.01 & ERA5", rtitle="V850-U200 reg IndR")
+# ===================================================
+con = axs[2, 0].contourf(
+    vor_CRU_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    vor_CRU_India_pvalue,
+    axs[2, 0],
+    n,
+    np.where(vor_CRU_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[2, 0].format(ltitle="CRU TS4.01 & ERA5", rtitle="vorticity reg IndR")
+# ===================================================
+con = axs[0, 1].contourf(
+    ushear_GPCP_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    ushear_GPCP_India_pvalue,
+    axs[0, 1],
+    n,
+    np.where(ushear_GPCP_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[0, 1].format(ltitle="GPCP & ERA5", rtitle="U850-U200 reg IndR")
+# ===================================================
+con = axs[1, 1].contourf(
+    vshear_GPCP_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    vshear_GPCP_India_pvalue,
+    axs[1, 1],
+    n,
+    np.where(vshear_GPCP_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[1, 1].format(ltitle="GPCP & ERA5", rtitle="V850-U200 reg IndR")
+# ===================================================
+con = axs[2, 1].contourf(
+    vor_GPCP_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    vor_GPCP_India_pvalue,
+    axs[2, 1],
+    n,
+    np.where(vor_GPCP_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[2, 1].format(ltitle="GPCP & ERA5", rtitle="vorticity reg IndR")
+# ===================================================
+con = axs[0, 2].contourf(
+    ushear_his_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    ushear_his_India_pvalue,
+    axs[0, 2],
+    n,
+    np.where(ushear_his_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[0, 2].format(ltitle="historical & ERA5", rtitle="U850-U200 reg IndR")
+# ===================================================
+con = axs[1, 2].contourf(
+    vshear_his_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    vshear_his_India_pvalue,
+    axs[1, 2],
+    n,
+    np.where(vshear_his_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[1, 2].format(ltitle="historical & ERA5", rtitle="V850-U200 reg IndR")
+# ===================================================
+con = axs[2, 2].contourf(
+    vor_his_India_rvalue,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+)
+sepl.plt_sig(
+    vor_his_India_pvalue,
+    axs[2, 2],
+    n,
+    np.where(vor_his_India_pvalue[::n, ::n] <= 0.05),
+    "denim",
+    3.0,
+)
+axs[2, 2].format(ltitle="historical & ERA5", rtitle="vorticity reg IndR")
+
+
+
+
+fig.colorbar(con, loc="b", width=0.13, length=0.7, label="")
+fig.format(abc="(a)", abcloc="l")
 # %%
