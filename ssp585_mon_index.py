45147b3c046a3aab3c4125fd3d41b734a8b09eaa
Hongjing0506 <18520228343@163.com>
2022-04-13 20:14:47 +0800

    calculate the SAM and IWF index in ERA5, historical and ssp585

--- ssp585_mon_index.py
+++ ssp585_mon_index.py
@@ -60,7 +60,7 @@ def patches(ax, x0, y0, width, height, proj):
 
 
 # %%
-#   read obs and historical data
+#   read obs data
 fhgtERA5 = xr.open_dataset(
     "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/hgt_mon_r144x72_195001-201412.nc"
 )
@@ -85,39 +85,6 @@ spERA5 = fspERA5["sp"]
 fqERA5 = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/q_mon_r144x72_195001-201412.nc")
 qERA5 = fqERA5["q"]
 
-fhgthis = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/historical/zg/zg_Amon_ensemble_historical_gn_195001-201412.nc"
-)
-hgthis = fhgthis["zg"]
-hgthis.coords["plev"] = hgthis.coords["plev"] / 100.0
-hgthis = hgthis.rename({"plev": "level"})
-
-fuhis = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/historical/ua/ua_Amon_ensemble_historical_gn_195001-201412.nc"
-)
-uhis = fuhis["ua"]
-uhis.coords["plev"] = uhis.coords["plev"] / 100.0
-uhis = uhis.rename({"plev": "level"})
-
-fvhis = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/historical/va/va_Amon_ensemble_historical_gn_195001-201412.nc"
-)
-vhis = fvhis["va"]
-vhis.coords["plev"] = vhis.coords["plev"] / 100.0
-vhis = vhis.rename({"plev": "level"})
-
-fsphis = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/historical/ps/ps_Amon_ensemble_historical_gn_195001-201412.nc"
-)
-sphis = fsphis["ps"]
-
-fqhis = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/historical/hus/hus_Amon_ensemble_historical_gn_195001-201412.nc"
-)
-qhis = fqhis["hus"]
-qhis.coords["plev"] = qhis.coords["plev"] / 100.0
-qhis = qhis.rename({"plev": "level"})
-
 # %%
 #   read the precipitation data
 fpreCRU = xr.open_dataset(
@@ -125,12 +92,10 @@ fpreCRU = xr.open_dataset(
 )
 preCRU = fpreCRU["pre"]
 
-
-fprehis = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/historical/pr/pr_Amon_ensemble_historical_gn_195001-201412.nc"
-)
-prehis = fprehis["pr"] * 3600 * 24
-
+# fprehis = xr.open_dataset(
+#     "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/historical/pr/pr_Amon_ensemble_historical_gn_195001-201412.nc"
+# )
+# prehis = fprehis["pr"] * 3600 * 24
 
 # GPCP data just have 1979-2014 year
 fpreGPCP = xr.open_dataset(
@@ -154,96 +119,28 @@ qERA5_ver_JJA = ca.detrend_dim(qERA5_ver_JJA, "time", deg=1, demean=False)
 preCRU_JJA = ca.detrend_dim(preCRU_JJA, "time", deg=1, demean=False)
 spERA5_JJA = ca.detrend_dim(spERA5_JJA, "time", deg=1, demean=False)
 
-hgthis_ver_JJA = ca.p_time(hgthis, 6, 8, True).loc[:, :100, :, :]
-
-uhis_ver_JJA = ca.p_time(uhis, 6, 8, True).loc[:, :100, :, :]
-vhis_ver_JJA = ca.p_time(vhis, 6, 8, True).loc[:, :100, :, :]
-qhis_ver_JJA = ca.p_time(qhis, 6, 8, True).loc[:, :100, :, :]
-prehis_JJA = ca.p_time(prehis, 6, 8, True)
-sphis_JJA = ca.p_time(sphis, 6, 8, True)
-
-hgthis_ver_JJA = ca.detrend_dim(hgthis_ver_JJA, "time", deg=1, demean=False)
-uhis_ver_JJA = ca.detrend_dim(uhis_ver_JJA, "time", deg=1, demean=False)
-vhis_ver_JJA = ca.detrend_dim(vhis_ver_JJA, "time", deg=1, demean=False)
-qhis_ver_JJA = ca.detrend_dim(qhis_ver_JJA, "time", deg=1, demean=False)
-prehis_JJA = ca.detrend_dim(prehis_JJA, "time", deg=1, demean=False)
-sphis_JJA = ca.detrend_dim(sphis_JJA, "time", deg=1, demean=False)
-
 preGPCP_JJA = ca.p_time(preGPCP, 6, 8, True)
 preGPCP_JJA = ca.detrend_dim(preGPCP_JJA, "time", deg=1, demean=False)
-# %%
-#   read the ssp585 data
-fhgtssp585 = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/ssp585/zg/zg_Amon_ensemble_ssp585_gn_201501-209912.nc"
-)
-hgtssp585 = fhgtssp585["zg"]
-hgtssp585.coords["plev"] = hgtssp585.coords["plev"] / 100.0
-hgtssp585 = hgtssp585.rename({"plev": "level"})
-
-fussp585 = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/ssp585/ua/ua_Amon_ensemble_ssp585_gn_201501-209912.nc"
-)
-ussp585 = fussp585["ua"]
-ussp585.coords["plev"] = ussp585.coords["plev"] / 100.0
-ussp585 = ussp585.rename({"plev": "level"})
-
-fvssp585 = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/ssp585/va/va_Amon_ensemble_ssp585_gn_201501-209912.nc"
-)
-vssp585 = fvssp585["va"]
-vssp585.coords["plev"] = vssp585.coords["plev"] / 100.0
-vssp585 = vssp585.rename({"plev": "level"})
-
-fspssp585 = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/ssp585/ps/ps_Amon_ensemble_ssp585_gn_201501-209912.nc"
-)
-spssp585 = fspssp585["ps"]
-
-fqssp585 = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/ssp585/hus/hus_Amon_ensemble_ssp585_gn_201501-209912.nc"
-)
-qssp585 = fqssp585["hus"]
-qssp585.coords["plev"] = qssp585.coords["plev"] / 100.0
-qssp585 = qssp585.rename({"plev": "level"})
-
-fpressp585 = xr.open_dataset(
-    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/pick_models/ssp585/pr/pr_Amon_ensemble_ssp585_gn_201501-209912.nc"
-)
-pressp585 = fpressp585["pr"] * 3600 * 24
-# %%
-#   pick up the ssp585 data
-hgtssp585_ver_JJA = ca.p_time(hgtssp585, 6, 8, True).loc[:, :100, :, :]
-
-ussp585_ver_JJA = ca.p_time(ussp585, 6, 8, True).loc[:, :100, :, :]
-vssp585_ver_JJA = ca.p_time(vssp585, 6, 8, True).loc[:, :100, :, :]
-qssp585_ver_JJA = ca.p_time(qssp585, 6, 8, True).loc[:, :100, :, :]
-pressp585_JJA = ca.p_time(pressp585, 6, 8, True)
-spssp585_JJA = ca.p_time(spssp585, 6, 8, True)
-
-hgtssp585_ver_JJA = ca.detrend_dim(hgtssp585_ver_JJA, "time", deg=1, demean=False)
-ussp585_ver_JJA = ca.detrend_dim(ussp585_ver_JJA, "time", deg=1, demean=False)
-vssp585_ver_JJA = ca.detrend_dim(vssp585_ver_JJA, "time", deg=1, demean=False)
-qssp585_ver_JJA = ca.detrend_dim(qssp585_ver_JJA, "time", deg=1, demean=False)
-pressp585_JJA = ca.detrend_dim(pressp585_JJA, "time", deg=1, demean=False)
-spssp585_JJA = ca.detrend_dim(spssp585_JJA, "time", deg=1, demean=False)
 
 # %%
 #   calculate the SAM and IWF index in ERA5, historical, ssp585
-reload(ca)
+
 ERA5_IWF_index = ca.IWF(uERA5_ver_JJA, vERA5_ver_JJA)
 ERA5_IWF_index = ca.detrend_dim(ERA5_IWF_index, "time", deg=1, demean=False)
 ERA5_SAM_index = ca.SAM(vERA5_ver_JJA)
 ERA5_SAM_index = ca.detrend_dim(ERA5_SAM_index, "time", deg=1, demean=False)
 
-his_IWF_index = ca.IWF(uhis_ver_JJA, vhis_ver_JJA)
-his_IWF_index = ca.detrend_dim(his_IWF_index, "time", deg=1, demean=False)
-his_SAM_index = ca.SAM(vhis_ver_JJA)
-his_SAM_index = ca.detrend_dim(his_SAM_index, "time", deg=1, demean=False)
+fhis_IWF_index = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/his_IWF_index_1950-2014.nc")
+his_IWF_index = fhis_IWF_index["IWF"]
 
-ssp585_IWF_index = ca.IWF(ussp585_ver_JJA, vssp585_ver_JJA)
-ssp585_IWF_index = ca.detrend_dim(ssp585_IWF_index, "time", deg=1, demean=False)
-ssp585_SAM_index = ca.SAM(vssp585_ver_JJA)
-ssp585_SAM_index = ca.detrend_dim(ssp585_SAM_index, "time", deg=1, demean=False)
+fhis_SAM_index = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/his_SAM_index_1950-2014.nc")
+his_SAM_index = fhis_SAM_index["SAM"]
+
+fssp585_IWF_index = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/ssp585_IWF_index_2015-2099.nc")
+ssp585_IWF_index = fssp585_IWF_index["IWF"]
+
+fssp585_SAM_index = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/ssp585_SAM_index_2015-2099.nc")
+ssp585_SAM_index = fssp585_SAM_index["SAM"]
 # %%
 print(stats.linregress(ssp585_IWF_index, ssp585_SAM_index))
 # %%
