1f61e771d218d8b32df61149ed9f13386ab1ca0d
Hongjing0506 <18520228343@163.com>
2022-04-14 16:58:30 +0800

    calculate the precipitation regress onto India precipitation

--- cal_pre_regress.py
+++ cal_pre_regress.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-04-14 16:32:41
 LastEditors: ChenHJ
-LastEditTime: 2022-04-14 16:47:08
+LastEditTime: 2022-04-14 16:57:47
 FilePath: /chenhj/0302code/cal_pre_regress.py
 Aim: 
 Mission: 
@@ -63,11 +63,50 @@ fpreCRU = xr.open_dataset(
     "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/cru_ts4.01_r144x72_195001-201412.nc"
 )
 preCRU = fpreCRU["pre"]
+preCRU_JJA = ca.p_time(preCRU, 6, 8, True) / 30.67
+preCRU_JJA = ca.detrend_dim(preCRU_JJA, "time", deg=1, demean=False)
 
 fprehis = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/pr_historical_r144x72_195001-201412.nc")
-prehis = fprehis["pr"] * 3600 * 24
+prehis_JJA = fprehis["pr"] * 3600 * 24
 
 fpressp585 = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/pr_ssp585_r144x72_201501-209912.nc")
-pressp585 = fpressp585["pr"] * 3600 *24
+pressp585_JJA = fpressp585["pr"] * 3600 *24
+
+# %%
+#   calculate the precipitation regress onto India precipitation
+preCRU_India_JJA = ca.cal_lat_weighted_mean(preCRU_JJA.loc[:, 8:28, 70:86]).mean(dim="lon", skipna=True)
+
+lat = prehis.coords["lat"]
+lon = prehis.coords["lon"]
+
+lat_India_range = lat[(lat >= 8.0) & (lat <= 28.0)]
+lon_India_range = lon[(lon >= 70.0) & (lon <= 86.0)]
+
+prehis_India_JJA = ca.cal_lat_weighted_mean(prehis_JJA.sel(lat=lat_India_range, lon=lon_India_range)).mean(dim="lon", skipna=True)
+pressp585_India_JJA = ca.cal_lat_weighted_mean(pressp585_JJA.sel(lat=lat_India_range, lon=lon_India_range)).mean(dim="lon", skipna=True)
+
+# %%
+(
+    pre_CRU_India_pre_slope,
+    pre_CRU_India_pre_intercept,
+    pre_CRU_India_pre_rvalue,
+    pre_CRU_India_pre_pvalue,
+    pre_CRU_India_pre_hypothesis,
+) = ca.dim_linregress(preCRU_India_JJA, preCRU_JJA)
+
+(
+    pre_his_India_pre_slope,
+    pre_his_India_pre_intercept,
+    pre_his_India_pre_rvalue,
+    pre_his_India_pre_pvalue,
+    pre_his_India_pre_hypothesis,
+) = ca.dim_linregress(prehis_India_JJA, prehis_JJA)
+(
+    pre_ssp585_India_pre_slope,
+    pre_ssp585_India_pre_intercept,
+    pre_ssp585_India_pre_rvalue,
+    pre_ssp585_India_pre_pvalue,
+    pre_ssp585_India_pre_hypothesis,
+) = ca.dim_linregress(pressp585_India_JJA, preCRU_JJA)
 
 # %%
