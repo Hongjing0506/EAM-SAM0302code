d5527e345de3af7987aad63674174f42a5f93a42
Hongjing0506 <18520228343@163.com>
2022-04-14 18:20:38 +0800

    plot the regression coefficients of CRU, historical ensmean, ssp585 ensmean, diff:ssp585-historical

--- cal_pre_regress.py
+++ cal_pre_regress.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-04-14 16:32:41
 LastEditors: ChenHJ
-LastEditTime: 2022-04-14 17:48:39
+LastEditTime: 2022-04-14 18:20:30
 FilePath: /chenhj/0302code/cal_pre_regress.py
 Aim: 
 Mission: 
@@ -280,4 +280,115 @@ for num_models,mod in enumerate(pre_ssp585_India_pre_slope.coords["models"].data
 fig.colorbar(con, loc="b", width=0.13, length=0.7, label="")
 fig.format(abc="(a)", abcloc="l", suptitle="pre reg IndR")
 # %%
-gmodels = [""]
+# pick_up the good models
+gmodels = ["CNRM-CM6-1", "MIROC-ES2L", "NorESM2-LM", "HadGEM3-GC31-LL", "MRI-ESM2-0", "ACCESS-CM2", "MIROC6", "EC-Earth3", "CESM2-WACCM", "CAMS-CSM1-0"]
+prehis_JJA_gmodels = prehis_JJA.sel(models=gmodels)
+pressp_JJA_gmodels = pressp585_JJA.sel(models=gmodels)
+pre_his_India_pre_slope_gmodels = pre_his_India_pre_slope.sel(models=gmodels)
+pre_ssp585_India_pre_slope_gmodels = pre_ssp585_India_pre_slope.sel(models=gmodels)
+pre_his_India_pre_pvalue_gmodels = pre_his_India_pre_pvalue.sel(models=gmodels)
+pre_ssp585_India_pre_pvalue_gmodels = pre_ssp585_India_pre_pvalue.sel(models=gmodels)
+
+pre_his_India_pre_slope_gmodels_ens = pre_his_India_pre_slope_gmodels.mean(dim="models", skipna=True)
+pre_ssp585_India_pre_slope_gmodels_ens = pre_ssp585_India_pre_slope_gmodels.mean(dim="models", skipna=True)
+
+pre_his_India_pre_slope_gmodels_ens_mask = ca.MME_reg_mask(pre_his_India_pre_slope_gmodels_ens, pre_his_India_pre_slope_gmodels.std(dim="models", skipna=True), len(gmodels), True)
+pre_ssp585_India_pre_slope_gmodels_ens_mask = ca.MME_reg_mask(pre_ssp585_India_pre_slope_gmodels_ens, pre_ssp585_India_pre_slope_gmodels.std(dim="models", skipna=True), len(gmodels), True)
+# %%
+#   plot the regression coefficients of CRU, historical ensmean, ssp585 ensmean, diff:ssp585-historical
+pplt.rc.grid = False
+pplt.rc.reso = "lo"
+cl = 0  # 设置地图投影的中心纬度
+proj = pplt.PlateCarree(central_longitude=cl)
+
+fig = pplt.figure(span=False, share=False, refwidth=4.0, wspace=4.0, hspace=3.5, outerpad=2.0)
+axs = fig.subplots(ncols=1, nrows=4, proj=proj)
+
+#   set the geo_ticks and map projection to the plots
+xticks = np.array([30, 60, 90, 120, 150, 180])  # 设置纬度刻度
+yticks = np.arange(-30, 46, 15)  # 设置经度刻度
+# 设置绘图的经纬度范围extents，其中前两个参数为经度的最小值和最大值，后两个数为纬度的最小值和最大值
+# 当想要显示的经纬度范围不是正好等于刻度显示范围时，对extents进行相应的修改即可
+extents = [xticks[0], xticks[-1], yticks[0], 55.0]
+sepl.geo_ticks(axs, xticks, yticks, cl, 10, 5, extents)
+
+# ===================================================
+ski = 2
+n = 1
+w, h = 0.12, 0.14
+# ===================================================
+for ax in axs:
+    # Inida area
+    x0 = 70
+    y0 = 8.0
+    width = 16
+    height = 20.0
+    patches(ax, x0 - cl, y0, width, height, proj)
+# ===================================================
+con = axs[0].contourf(
+    pre_CRU_India_pre_slope,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-2.0, 2.1, 0.1),
+    zorder=0.8,
+    extend="both",
+    )
+sepl.plt_sig(
+    pre_CRU_India_pre_slope, axs[0], n, np.where(pre_CRU_India_pre_pvalue[::n, ::n] <= 0.05), "denim", 3.0,
+)
+
+axs[0].format(
+    rtitle="1950-2014", ltitle="CRU",
+)
+# ===================================================
+con = axs[1].contourf(
+    pre_his_India_pre_slope_gmodels_ens,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-2.0, 2.1, 0.1),
+    zorder=0.8,
+    extend="both",
+    )
+sepl.plt_sig(
+    pre_his_India_pre_slope_gmodels_ens, axs[1], n, np.where(pre_his_India_pre_slope_gmodels_ens_mask[::n, ::n] > 0.0), "denim", 3.0,
+)
+
+axs[1].format(
+    rtitle="1950-2014", ltitle="historical ensmean",
+)
+# ===================================================
+con = axs[2].contourf(
+    pre_ssp585_India_pre_slope_gmodels_ens,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-2.0, 2.1, 0.1),
+    zorder=0.8,
+    extend="both",
+    )
+sepl.plt_sig(
+    pre_ssp585_India_pre_slope_gmodels_ens, axs[2], n, np.where(pre_ssp585_India_pre_slope_gmodels_ens_mask[::n, ::n] > 0.0), "denim", 3.0,
+)
+
+axs[2].format(
+    rtitle="2015-2099", ltitle="ssp585 ensmean",
+)
+# ===================================================
+con = axs[3].contourf(
+    pre_ssp585_India_pre_slope_gmodels_ens - pre_his_India_pre_slope_gmodels_ens,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-0.5, 0.6, 0.05),
+    zorder=0.8,
+    extend="both",
+    )
+# sepl.plt_sig(
+#     pre_ssp585_India_pre_slope_gmodels_ens, axs[3], n, np.where(pre_ssp585_India_pre_slope_gmodels_ens_mask[::n, ::n] > 0.0), "denim", 3.0,
+# )
+
+axs[3].format(
+    rtitle="diff", ltitle="ssp585 - historical",
+)
+# ===================================================
+fig.colorbar(con, loc="b", width=0.13, length=0.7, label="")
+fig.format(abc="(a)", abcloc="l", suptitle="pre reg IndR")
+# %%
