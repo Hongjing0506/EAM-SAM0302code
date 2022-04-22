cb94d71936703db488d72d0ba2b14d346f46a61f
Hongjing0506 <18520228343@163.com>
2022-04-22 20:46:25 +0800

    calculate the precipitation regress onto BOB precipitation

--- cal_pre_regress.py
+++ cal_pre_regress.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-04-14 16:32:41
 LastEditors: ChenHJ
-LastEditTime: 2022-04-22 20:38:25
+LastEditTime: 2022-04-22 20:44:01
 FilePath: /chenhj/0302code/cal_pre_regress.py
 Aim: 
 Mission: 
@@ -2627,13 +2627,13 @@ print(sorted(IndR_EAM_list, key=lambda x : x["pcc"]))
 #   calculate the BOB precipitation
 lat = preCRU_JJA.coords["lat"]
 lon = preCRU_JJA.coords["lon"]
-lat_BOB_range = lat[(lat >= 10.0) & (lat <= 30.0)]
+lat_BOB_range = lat[(lat >= 10.0) & (lat <= 25.0)]
 lon_BOB_range = lon[(lon >= 80.0) & (lon <= 100.0)]
 
 preCRU_BOB_JJA = ca.cal_lat_weighted_mean(preCRU_JJA.sel(lat=lat_BOB_range, lon=lon_BOB_range)).mean(dim="lon", skipna=True)
 lat = preGPCP_JJA.coords["lat"]
 lon = preGPCP_JJA.coords["lon"]
-lat_BOB_range = lat[(lat >= 10.0) & (lat <= 30.0)]
+lat_BOB_range = lat[(lat >= 10.0) & (lat <= 25.0)]
 lon_BOB_range = lon[(lon >= 80.0) & (lon <= 100.0)]
 # lat_BOB_range = lat[(lat >= 15.0) & (lat <= 28.0)]
 # lat_BOB_range = lat[(lat >= 30.0) & (lat <= 35.0)]
@@ -2644,7 +2644,7 @@ preGPCP_BOB_JJA = ca.cal_lat_weighted_mean(preGPCP_JJA.sel(lat=lat_BOB_range, lo
 lat = prehis_JJA.coords["lat"]
 lon = prehis_JJA.coords["lon"]
 
-lat_BOB_range = lat[(lat >= 10.0) & (lat <= 30.0)]
+lat_BOB_range = lat[(lat >= 10.0) & (lat <= 25.0)]
 # lat_BOB_range = lat[(lat >= 15.0) & (lat <= 28.0)]
 lon_BOB_range = lon[(lon >= 80.0) & (lon <= 100.0)]
 # lat_BOB_range = lat[(lat >= 30.0) & (lat <= 35.0)]
@@ -2671,8 +2671,134 @@ pressp585_BOB_JJA = ca.cal_lat_weighted_mean(pressp585_JJA.sel(lat=lat_BOB_range
     pre_GPCP_BOB_pre_hypothesis,
 ) = ca.dim_linregress(preGPCP_BOB_JJA, preGPCP_JJA)
 
+(
+    pre_his_BOB_pre_slope,
+    pre_his_BOB_pre_intercept,
+    pre_his_BOB_pre_rvalue,
+    pre_his_BOB_pre_pvalue,
+    pre_his_BOB_pre_hypothesis,
+) = ca.dim_linregress(prehis_BOB_JJA.sel(time=prehis_BOB_JJA.time.dt.year>=1979), prehis_JJA.sel(time=prehis_JJA.time.dt.year>=1979))
+(
+    pre_ssp585_BOB_pre_slope,
+    pre_ssp585_BOB_pre_intercept,
+    pre_ssp585_BOB_pre_rvalue,
+    pre_ssp585_BOB_pre_pvalue,
+    pre_ssp585_BOB_pre_hypothesis,
+) = ca.dim_linregress(pressp585_BOB_JJA, pressp585_JJA)
+# %%
+pre_his_BOB_pre_slope_ens = pre_his_BOB_pre_slope.mean(dim="models", skipna=True)
+pre_ssp585_BOB_pre_slope_ens = pre_ssp585_BOB_pre_slope.mean(dim="models", skipna=True)
+pre_his_BOB_pre_slope_ens_mask = ca.MME_reg_mask(pre_his_BOB_pre_slope_ens, pre_his_BOB_pre_slope.std(dim="models", skipna=True), len(pre_his_BOB_pre_slope.coords["models"]), True)
+pre_ssp585_BOB_pre_slope_ens_mask = ca.MME_reg_mask(pre_ssp585_BOB_pre_slope_ens, pre_ssp585_BOB_pre_slope.std(dim="models", skipna=True), len(pre_ssp585_BOB_pre_slope.coords["models"]), True)
+
+pre_his_BOB_pre_rvalue_ens = pre_his_BOB_pre_rvalue.mean(dim="models", skipna=True)
+pre_ssp585_BOB_pre_rvalue_ens = pre_ssp585_BOB_pre_rvalue.mean(dim="models", skipna=True)
+pre_his_BOB_pre_rvalue_ens_mask = ca.MME_reg_mask(pre_his_BOB_pre_rvalue_ens, pre_his_BOB_pre_rvalue.std(dim="models", skipna=True), len(pre_his_BOB_pre_rvalue.coords["models"]), True)
+pre_ssp585_BOB_pre_rvalue_ens_mask = ca.MME_reg_mask(pre_ssp585_BOB_pre_rvalue_ens, pre_ssp585_BOB_pre_rvalue.std(dim="models", skipna=True), len(pre_ssp585_BOB_pre_rvalue.coords["models"]), True)
+
 # %%
 #   plot the regression coefficients in CRU, GPCP, historical run
+pplt.rc.grid = False
+pplt.rc.reso = "lo"
+cl = 0  # 设置地图投影的中心纬度
+proj = pplt.PlateCarree(central_longitude=cl)
+
+fig = pplt.figure(span=False, share=False, refwidth=4.0, wspace=4.0, hspace=3.5, outerpad=2.0)
+# plot_array = np.reshape(range(1, 31), (6, 5))
+# plot_array[5,-1] = 0
+axs = fig.subplots(ncols=1, nrows=2, proj=proj)
+
+#   set the geo_ticks and map projection to the plots
+xticks = np.array([30, 60, 90, 120, 150, 180])  # 设置纬度刻度
+yticks = np.arange(-30, 46, 15)  # 设置经度刻度
+# 设置绘图的经纬度范围extents，其中前两个参数为经度的最小值和最大值，后两个数为纬度的最小值和最大值
+# 当想要显示的经纬度范围不是正好等于刻度显示范围时，对extents进行相应的修改即可
+extents = [xticks[0], xticks[-1], yticks[0], 55.0]
+sepl.geo_ticks(axs, xticks, yticks, cl, 10, 5, extents)
+
+# ===================================================
+ski = 2
+n = 1
+w, h = 0.12, 0.14
+# ===================================================
+for ax in axs:
+    # BOB area
+    x0 = 80
+    y0 = 10.0
+    width = 20.0
+    height = 15.0
+    patches(ax, x0 - cl, y0, width, height, proj)
+# ===================================================
+con = axs[0].contourf(
+    pre_CRU_BOB_pre_slope,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-2.0, 2.1, 0.1),
+    zorder=0.8,
+    extend="both",
+    )
+sepl.plt_sig(
+    pre_CRU_BOB_pre_slope, axs[0], n, np.where(pre_CRU_BOB_pre_pvalue[::n, ::n] <= 0.05), "bright purple", 4.0,
+)
+
+axs[0].format(
+    rtitle="1979-2014", ltitle="CRU",
+)
+# ===================================================
+con = axs[1].contourf(
+    pre_GPCP_BOB_pre_slope,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-2.0, 2.1, 0.1),
+    zorder=0.8,
+    extend="both",
+    )
+sepl.plt_sig(
+    pre_GPCP_BOB_pre_slope, axs[1], n, np.where(pre_GPCP_BOB_pre_pvalue[::n, ::n] <= 0.05), "bright purple", 4.0,
+)
+
+axs[1].format(
+    rtitle="1979-2014", ltitle="GPCP",
+)
+# ===================================================
+# ===================================================
+con = axs[2].contourf(
+    pre_his_India_pre_slope_ens,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-2.0, 2.1, 0.1),
+    zorder=0.8,
+    extend="both",
+    )
+sepl.plt_sig(
+    pre_his_India_pre_slope_ens, axs[2], n, np.where(pre_his_India_pre_slope_ens_mask[::n, ::n] > 0.0), "bright purple", 4.0,
+)
+
+axs[2].format(
+    rtitle="1979-2014", ltitle="MME",
+)
+# ===================================================
+for num_models,mod in enumerate(pre_his_India_pre_slope.coords["models"].data):
+    con = axs[num_models+3].contourf(
+    pre_his_India_pre_slope.sel(models=mod),
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-2.0, 2.1, 0.1),
+    zorder=0.8,
+    extend="both",
+    )
+    sepl.plt_sig(
+        pre_his_India_pre_slope.sel(models=mod), axs[num_models+3], n, np.where(pre_his_India_pre_pvalue.sel(models=mod)[::n, ::n] <= 0.05), "bright purple", 4.0,
+    )
+
+    axs[num_models+3].format(
+        rtitle="1979-2014", ltitle="{}".format(mod),
+    )
+fig.colorbar(con, loc="b", width=0.13, length=0.7, label="")
+fig.format(abc="(a)", abcloc="l", suptitle="pre reg IndR")
+
+# %%
+#   plot the correlation coefficients rvalue in CRU, GPCP, historical run
 pplt.rc.grid = False
 pplt.rc.reso = "lo"
 cl = 0  # 设置地图投影的中心纬度
@@ -2705,15 +2831,14 @@ for ax in axs:
     patches(ax, x0 - cl, y0, width, height, proj)
 # ===================================================
 con = axs[0].contourf(
-    pre_CRU_India_pre_slope,
+    pre_CRU_India_pre_rvalue,
     cmap="ColdHot",
     cmap_kw={"left": 0.06, "right": 0.94},
-    levels=np.arange(-2.0, 2.1, 0.1),
+    levels=np.arange(-1.0, 1.1, 0.1),
     zorder=0.8,
-    extend="both",
     )
 sepl.plt_sig(
-    pre_CRU_India_pre_slope, axs[0], n, np.where(pre_CRU_India_pre_pvalue[::n, ::n] <= 0.05), "bright purple", 4.0,
+    pre_CRU_India_pre_rvalue, axs[0], n, np.where(pre_CRU_India_pre_pvalue[::n, ::n] <= 0.05), "bright purple", 4.0,
 )
 
 axs[0].format(
@@ -2721,21 +2846,53 @@ axs[0].format(
 )
 # ===================================================
 con = axs[1].contourf(
-    pre_GPCP_India_pre_slope,
+    pre_GPCP_India_pre_rvalue,
     cmap="ColdHot",
     cmap_kw={"left": 0.06, "right": 0.94},
-    levels=np.arange(-2.0, 2.1, 0.1),
+    levels=np.arange(-1.0, 1.1, 0.1),
     zorder=0.8,
-    extend="both",
     )
 sepl.plt_sig(
-    pre_GPCP_India_pre_slope, axs[1], n, np.where(pre_GPCP_India_pre_pvalue[::n, ::n] <= 0.05), "bright purple", 4.0,
+    pre_GPCP_India_pre_rvalue, axs[1], n, np.where(pre_GPCP_India_pre_pvalue[::n, ::n] <= 0.05), "bright purple", 4.0,
 )
 
 axs[1].format(
     rtitle="1979-2014", ltitle="GPCP",
 )
+# # ===================================================
+con = axs[2].contourf(
+    pre_his_India_pre_rvalue_ens,
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+    
+    )
+sepl.plt_sig(
+    pre_his_India_pre_rvalue_ens, axs[2], n, np.where(pre_his_India_pre_rvalue_ens_mask[::n, ::n] > 0.0), "bright purple", 4.0,
+)
+
+axs[2].format(
+    rtitle="1979-2014", ltitle="MME",
+)
 # ===================================================
+for num_models,mod in enumerate(pre_his_India_pre_slope.coords["models"].data):
+    con = axs[num_models+3].contourf(
+    pre_his_India_pre_rvalue.sel(models=mod),
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94},
+    levels=np.arange(-1.0, 1.1, 0.1),
+    zorder=0.8,
+    )
+    sepl.plt_sig(
+        pre_his_India_pre_rvalue.sel(models=mod), axs[num_models+3], n, np.where(pre_his_India_pre_pvalue.sel(models=mod)[::n, ::n] <= 0.05), "bright purple", 4.0,
+    )
+
+    axs[num_models+3].format(
+        rtitle="1979-2014", ltitle="{}".format(mod),
+    )
+fig.colorbar(con, loc="b", width=0.13, length=0.7, label="")
+fig.format(abc="(a)", abcloc="l", suptitle="pre reg IndR")
 # %%
 # # pick_up the good models
 # gmodels = ["CNRM-CM6-1", "MIROC-ES2L", "NorESM2-LM", "HadGEM3-GC31-LL", "MRI-ESM2-0", "ACCESS-CM2", "MIROC6", "EC-Earth3", "CESM2-WACCM", "CAMS-CSM1-0"]
