6a7e1dbef1193fdb188b6b4ffab1d60bf5dc21b8
Hongjing0506 <18520228343@163.com>
2022-04-22 15:33:17 +0800

    plot the climatology jet stream location

---
+++ jet.py
@@ -0,0 +1,138 @@
+'''
+Author: ChenHJ
+Date: 2022-04-22 12:17:06
+LastEditors: ChenHJ
+LastEditTime: 2022-04-22 12:51:02
+FilePath: /chenhj/0302code/jet.py
+Aim: 
+Mission: 
+'''
+# %%
+import numpy as np
+import xarray as xr
+import os
+import re
+from cdo import Cdo
+import shutil
+import sys
+
+sys.path.append("/home/ys17-23/chenhj/self_def/")
+import plot as sepl
+import cal as ca
+import pandas as pd
+from importlib import reload
+
+import metpy.calc as mpcalc
+import metpy.xarray as mpxr
+import metpy.constants as constants
+import geocat.comp
+from windspharm.xarray import VectorWind
+
+reload(sepl)
+
+# sd.path.append("/home/ys17-23/chenhj/1201code/self_def.py")
+
+cdo = Cdo()
+
+# for plot
+import proplot as pplt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.ticker import LongitudeFormatter
+from cartopy.mpl.ticker import LatitudeFormatter
+from cartopy.util import add_cyclic_point
+from matplotlib.ticker import MultipleLocator
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.stats import t
+from scipy import signal
+from eofs.multivariate.standard import MultivariateEof
+from eofs.standard import Eof
+from statsmodels.distributions.empirical_distribution import ECDF
+import dask
+
+
+def patches(ax, x0, y0, width, height, proj):
+    from matplotlib.patches import Rectangle
+
+    rect = Rectangle(
+        (x0, y0), width, height, fc="none", ec="grey7", linewidth=0.8, zorder=1.1, transform=proj, linestyle="--",
+    )
+    ax.add_patch(rect)
+# %%
+fhgtERA5 = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/hgt_mon_r144x72_195001-201412.nc"
+)
+hgtERA5 = fhgtERA5["z"]
+
+
+fuERA5 = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/uwind_mon_r144x72_195001-201412.nc"
+)
+uERA5 = fuERA5["u"]
+
+fvERA5 = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/vwind_mon_r144x72_195001-201412.nc"
+)
+vERA5 = fvERA5["v"]
+
+fspERA5 = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/sp_mon_r144x72_195001-201412.nc"
+)
+spERA5 = fspERA5["sp"]
+
+fqERA5 = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/q_mon_r144x72_195001-201412.nc")
+qERA5 = fqERA5["q"]
+
+# %%
+#   read the precipitation data
+fpreCRU = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/cru_ts4.01_r144x72_195001-201412.nc"
+)
+preCRU = fpreCRU["pre"]
+
+# GPCP data just have 1979-2014 year
+fpreGPCP = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/GPCP_r144x72_197901-201412.nc"
+)
+preGPCP = fpreGPCP["precip"]
+
+# %%
+hgtERA5_ver_JJA = ca.p_time(hgtERA5, 6, 8, True).loc[:, :, :, :]
+# hgtERA5_ver_JJA = hgtERA5_ver_JJA - hgtERA5_ver_JJA.mean(dim="lon", skipna=True)
+uERA5_ver_JJA = ca.p_time(uERA5, 6, 8, True).loc[:, :, :, :]
+vERA5_ver_JJA = ca.p_time(vERA5, 6, 8, True).loc[:, :, :, :]
+qERA5_ver_JJA = ca.p_time(qERA5, 6, 9, True).loc[:, :, :, :]
+preCRU_JJA = ca.p_time(preCRU, 6, 8, True) / 30.67
+spERA5_JJA = ca.p_time(spERA5, 6, 8, True)
+
+hgtERA5_ver_JJA = ca.detrend_dim(hgtERA5_ver_JJA, "time", deg=1, demean=False)
+uERA5_ver_JJA = ca.detrend_dim(uERA5_ver_JJA, "time", deg=1, demean=False)
+vERA5_ver_JJA = ca.detrend_dim(vERA5_ver_JJA, "time", deg=1, demean=False)
+qERA5_ver_JJA = ca.detrend_dim(qERA5_ver_JJA, "time", deg=1, demean=False)
+preCRU_JJA = ca.detrend_dim(preCRU_JJA, "time", deg=1, demean=False)
+spERA5_JJA = ca.detrend_dim(spERA5_JJA, "time", deg=1, demean=False)
+# %%
+lat = uERA5_ver_JJA.coords["lat"]
+lon = uERA5_ver_JJA.coords["lon"]
+lon_range = lon[(lon >= 110.0) & (lon <= 150.0)]
+uERA5_ver_JJA_EA = uERA5_ver_JJA.sel(lon=lon_range)
+uERA5_ver_JJA_EA_mean = uERA5_ver_JJA_EA.mean(dim=["time", "lon"], skipna=True)
+# %%
+
+fig = pplt.figure(span=False, share=False, refwidth=4.0, wspace=4.0, hspace=3.5, outerpad=2.0)
+axs = fig.subplots(ncols=1, nrows=1)
+
+# ===================================================
+con = axs[0].contourf(
+    uERA5_ver_JJA_EA_mean[::-1,:],
+    cmap="ColdHot",
+    cmap_kw={"left": 0.06, "right": 0.94, "cut": 0},
+    zorder=0.8,
+    levels=np.arange(-40, 41, 2),
+    extend="both"
+)
+axs.format(xlim=(-20, 60))
+
+# %%
