8d1cbc378b8739da16cb3ff84d3648df0bc85527
Hongjing0506 <18520228343@163.com>
2022-04-23 13:17:23 +0800

    read the data

--- cal_EUTT_IUTT_regress.py
+++ cal_EUTT_IUTT_regress.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-04-23 12:49:42
 LastEditors: ChenHJ
-LastEditTime: 2022-04-23 12:59:41
+LastEditTime: 2022-04-23 13:16:52
 FilePath: /chenhj/0302code/cal_EUTT_IUTT_regress.py
 Aim: 
 Mission: 
@@ -61,5 +61,82 @@ def patches(ax, x0, y0, width, height, proj):
     )
     ax.add_patch(rect)
 # %%
+#   read the data
+#   read the ERA5 reanalysis data
+fhgtERA5 = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/hgt_mon_r144x72_195001-201412.nc"
+)
+hgtERA5 = fhgtERA5["z"]
 
-/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/ta_historical_r144x72_195001-201412.nc
+
+fuERA5 = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/uwind_mon_r144x72_195001-201412.nc"
+)
+uERA5 = fuERA5["u"]
+
+fvERA5 = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/vwind_mon_r144x72_195001-201412.nc"
+)
+vERA5 = fvERA5["v"]
+
+ftERA5 = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/temp_mon_r144x72_195001-201412.nc")
+tERA5 = ftERA5["t"]
+
+#   read the precipitation data
+fpreCRU = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/cru_ts4.01_r144x72_195001-201412.nc"
+)
+preCRU = fpreCRU["pre"]
+preCRU = fpreCRU["pre"]
+preCRU_JJA = ca.p_time(preCRU, 6, 8, True)/30.67
+preCRU_JJA = ca.detrend_dim(preCRU_JJA, "time", deg=1, demean=False)
+preCRU_JJA.attrs["units"] = "mm/day"
+
+# GPCP data just have 1979-2014 year
+fpreGPCP = xr.open_dataset(
+    "/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/obs/GPCP_r144x72_197901-201412.nc"
+)
+preGPCP = fpreGPCP["precip"]
+preGPCP_JJA = ca.p_time(preGPCP, 6, 8, True)
+preGPCP_JJA = ca.detrend_dim(preGPCP_JJA, "time", deg=1, demean=False)
+
+#   historical and ssp585 data
+
+fhgthis_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/zg_historical_r144x72_195001-201412.nc")
+hgthis_ver_JJA = fhgthis_ver_JJA["zg"]
+hgthis_ver_JJA = hgthis_ver_JJA - hgthis_ver_JJA.mean(dim="lon", skipna=True)
+
+fuhis_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/ua_historical_r144x72_195001-201412.nc")
+uhis_ver_JJA = fuhis_ver_JJA["ua"]
+
+fvhis_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/va_historical_r144x72_195001-201412.nc")
+vhis_ver_JJA = fvhis_ver_JJA["va"]
+
+fthis_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/ta_historical_r144x72_195001-201412.nc")
+this_ver_JJA = fthis_ver_JJA["ta"]
+
+fprehis = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/historical/tmp_var/JJA/detrend/pr_historical_r144x72_195001-201412.nc")
+prehis_JJA = fprehis["pr"]
+prehis_JJA.attrs["units"] = "mm/day"
+prehis_JJA.attrs["standard_name"] = "precipitation"
+
+fhgtssp585_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/zg_ssp585_r144x72_201501-209912.nc")
+hgtssp585_ver_JJA = fhgtssp585_ver_JJA["zg"]
+hgtssp585_ver_JJA = hgtssp585_ver_JJA - hgtssp585_ver_JJA.mean(dim="lon", skipna=True)
+
+fussp585_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/ua_ssp585_r144x72_201501-209912.nc")
+ussp585_ver_JJA = fussp585_ver_JJA["ua"]
+
+fvssp585_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/va_ssp585_r144x72_201501-209912.nc")
+vssp585_ver_JJA = fvssp585_ver_JJA["va"]
+
+ftssp585_ver_JJA = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/ta_ssp585_r144x72_201501-209912.nc")
+tssp585_ver_JJA = ftssp585_ver_JJA["ta"]
+
+fpressp585 = xr.open_dataset("/home/ys17-23/Extension/personal-data/chenhj/SAM_EAM_data/CMIP6/ssp585/tmp_var/JJA/detrend/pr_ssp585_r144x72_201501-209912.nc")
+pressp585_JJA = fpressp585["pr"]
+pressp585_JJA.attrs["units"] = "mm/day"
+pressp585_JJA.attrs["standard_name"] = "precipitation"
+
+
+# %%
