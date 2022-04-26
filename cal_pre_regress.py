93814835b67a83a5b873e4708057d61939ada266
Hongjing0506 <18520228343@163.com>
2022-04-26 13:31:02 +0800

    Fisher-Z transformation

--- cal_pre_regress.py
+++ cal_pre_regress.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-04-14 16:32:41
 LastEditors: ChenHJ
-LastEditTime: 2022-04-26 11:41:28
+LastEditTime: 2022-04-26 13:29:11
 FilePath: /chenhj/0302code/cal_pre_regress.py
 Aim: 
 Mission: 
@@ -4085,18 +4085,18 @@ IndR_ssp585_NCR_regress = ca.dim_linregress(pressp585_India_JJA, pressp585_NC_JJ
 IndR_ssp585_p3_NCR_regress = ca.dim_linregress(pressp585_India_JJA.sel(time=pressp585_India_JJA.time.dt.year>=2064), pressp585_NC_JJA.sel(time=pressp585_NC_JJA.time.dt.year>=2064))
 
 IndR_diff_NCR_slope = IndR_ssp585_p3_NCR_regress[0]-IndR_his_NCR_regress[0]
-IndR_diff_NCR_rvalue = IndR_ssp585_p3_NCR_regress[2]-IndR_his_NCR_regress[2]
+IndR_diff_NCR_rvalue = ca.cal_rdiff(IndR_ssp585_p3_NCR_regress[2],IndR_his_NCR_regress[2])
 
 IndR_diff_NCR_slope_ens = IndR_diff_NCR_slope.mean(dim="models",skipna=True)
-IndR_diff_NCR_rvalue_ens = IndR_diff_NCR_rvalue.mean(dim="models",skipna=True)
+IndR_diff_NCR_rvalue_ens = ca.cal_rMME(IndR_diff_NCR_rvalue,"models")
 
 IndR_his_NCR_slope_ens = IndR_his_NCR_regress[0].mean(dim="models",skipna=True)
 IndR_ssp585_NCR_slope_ens = IndR_ssp585_NCR_regress[0].mean(dim="models",skipna=True)
 IndR_ssp585_p3_NCR_slope_ens = IndR_ssp585_p3_NCR_regress[0].mean(dim="models",skipna=True)
 
-IndR_his_NCR_rvalue_ens = IndR_his_NCR_regress[2].mean(dim="models",skipna=True)
-IndR_ssp585_NCR_rvalue_ens = IndR_ssp585_NCR_regress[2].mean(dim="models",skipna=True)
-IndR_ssp585_p3_NCR_rvalue_ens = IndR_ssp585_p3_NCR_regress[2].mean(dim="models",skipna=True)
+IndR_his_NCR_rvalue_ens = ca.cal_rMME(IndR_his_NCR_regress[2],"models")
+IndR_ssp585_NCR_rvalue_ens = ca.cal_rMME(IndR_ssp585_NCR_regress[2],"models")
+IndR_ssp585_p3_NCR_rvalue_ens = ca.cal_rMME(IndR_ssp585_p3_NCR_regress[2],"models")
 # %%
 #   plot the bar-plot for regression coefficients
 plot_data = np.zeros((27,3))
@@ -4137,6 +4137,7 @@ fig = pplt.figure(span=False, share=False, refheight=4.0, refwidth=12.0, wspace=
 axs = fig.subplots(ncols=1, nrows=1)
 m = axs[0].bar(models,plot_data,width=0.6,cycle="tab10",edgecolor="grey7")
 axs[0].axhline(0,lw=1.5,color="grey7")
+axs[0].axhline()
 
 axs[0].legend(handles=m, loc='ur', labels=["historical", "ssp585_p3", "diff"])
 axs[0].format(ylim=(-0.5,0.5),ylocator=np.arange(-0.5,0.6,0.1),xlocator=np.arange(0,27), xtickminor=False, ytickminor=False, grid=False, xrotation=45, xticklabelsize=12, tickwidth=1.5, ticklen=6.0, linewidth=1.5, edgecolor="grey8")
