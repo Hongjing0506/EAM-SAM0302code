5598aa34d00df149b5e41429caa05e1058d983c4
Hongjing0506 <18520228343@163.com>
2022-05-08 16:47:22 +0800

    plot the singular scatter-plot for good models for reg coeff.

--- choose_India_area.py
+++ choose_India_area.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-05-06 15:24:33
 LastEditors: ChenHJ
-LastEditTime: 2022-05-08 16:35:26
+LastEditTime: 2022-05-08 16:46:45
 FilePath: /chenhj/0302code/choose_India_area.py
 Aim: 
 Mission: 
@@ -2832,7 +2832,30 @@ axs[0].scatter(1.50, IndR_ssp585_p3_NC_regress[0].sel(models=gmodels).mean(dim="
 for num_models, mod in enumerate(gmodels):
     axs[0].scatter(1.50, IndR_ssp585_p3_NC_regress[0].sel(models=mod), cycle=cycle, marker=".", markersize=100)
 
-# axs[0].set_xticks([0, 0.75, 1.5, 2.25])
-# axs[0].set_xticklabels(["", "1979-2014", "2064-2099", ""])
-axs[0].format(xlocator=[0.75, 1.5], ylim=(-0.2,0.5), xlim=(0,2.25), xformatter=["1979-2014", "2064-2099"], tickminor=False)
+axs[0].format(xlocator=[0.75, 1.5], ylim=(-0.2,0.5), xlim=(0,2.25), xformatter=["historical", "ssp585_p3"], tickminor=False)
+fig.format(suptitle="NC reg IndR")
+# %%
+#   plot the singular scatter-plot for good models for reg coeff.
+fig = pplt.figure(span=False, share=False, refheight=4.0, refwidth=4.0, wspace=4.0, hspace=3.5, outerpad=2.0)
+axs = fig.subplots(ncols=1, nrows=1)
+# cycle = pplt.Cycle('blues', 'acton', 'oranges', 'greens', 8, left=0.5)
+cycle = pplt.Cycle("Qual1", 7)
+
+#   hist-GPCP
+axs[0].scatter(0.75, IndR_GPCP_NC_regress[2],marker="s", labels="GPCP", legend_kw={"ncols":4}, color="blue5", legend="b")
+#   hist-gMME
+axs[0].scatter(0.75, IndR_his_NC_regress[2].sel(models=gmodels).mean(dim="models", skipna=True), marker="*", labels="gMME", legend_kw={"ncols":4}, cycle=cycle, legend="b", markersize=100)
+#   hist-gmodels
+for num_models, mod in enumerate(gmodels):
+    axs[0].scatter(0.75, IndR_his_NC_regress[2].sel(models=mod), cycle=cycle, legend="b", legend_kw={"ncols":4}, labels=mod, marker=".", markersize=100)
+    
+#   ssp585_p3-gMME
+axs[0].scatter(1.50, IndR_ssp585_p3_NC_regress[2].sel(models=gmodels).mean(dim="models", skipna=True), marker="*", cycle=cycle, markersize=100)
+
+#   ssp585_p3-gmodels
+for num_models, mod in enumerate(gmodels):
+    axs[0].scatter(1.50, IndR_ssp585_p3_NC_regress[2].sel(models=mod), cycle=cycle, marker=".", markersize=100)
+
+axs[0].format(xlocator=[0.75, 1.5], ylim=(-0.2,0.6), xlim=(0,2.25), xformatter=["historical", "ssp585_p3"], tickminor=False)
+fig.format(suptitle="NC reg IndR")
 # %%
