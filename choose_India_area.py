5b21f2bc035cbc867f8fdee353e2867c636e48d8
Hongjing0506 <18520228343@163.com>
2022-05-19 21:36:28 +0800

    plot the intermodels regression results

--- choose_India_area.py
+++ choose_India_area.py
@@ -2,7 +2,7 @@
 Author: ChenHJ
 Date: 2022-05-06 15:24:33
 LastEditors: ChenHJ
-LastEditTime: 2022-05-19 15:52:29
+LastEditTime: 2022-05-19 17:23:21
 FilePath: /chenhj/0302code/choose_India_area.py
 Aim: 
 Mission: 
@@ -5007,26 +5007,130 @@ for num_lev,lev in enumerate([200.0, 500.0, 850.0]):
 #   calculate the circulation result regress onto corr and slope between models
 
 (
-    IndR_NC_intermodels_slope_his_hgt_slope,
-    IndR_NC_intermodels_slope_his_hgt_intercept,
-    IndR_NC_intermodels_slope_his_hgt_rvalue,
-    IndR_NC_intermodels_slope_his_hgt_pvalue,
-    IndR_NC_intermodels_slope_his_hgt_hypothesis,
-) = ca.dim_linregress(IndR_his_NC_regress[2].sel(models=pcorr_models), IndR_his_hgt_slope.sel(models=pcorr_models), input_core_dims="models")
+    IndR_NC_intermodels_rvalue_his_hgt_slope,
+    IndR_NC_intermodels_rvalue_his_hgt_intercept,
+    IndR_NC_intermodels_rvalue_his_hgt_rvalue,
+    IndR_NC_intermodels_rvalue_his_hgt_pvalue,
+    IndR_NC_intermodels_rvalue_his_hgt_hypothesis,
+) = ca.dim_linregress(IndR_his_NC_regress[2].sel(models=pcorr_models), IndR_his_hgt_rvalue.sel(models=pcorr_models), input_core_dims="models")
 
 (
-    IndR_NC_intermodels_slope_his_u_slope,
-    IndR_NC_intermodels_slope_his_u_intercept,
-    IndR_NC_intermodels_slope_his_u_rvalue,
-    IndR_NC_intermodels_slope_his_u_pvalue,
-    IndR_NC_intermodels_slope_his_u_hypothesis,
-) = ca.dim_linregress(IndR_his_NC_regress[2].sel(models=pcorr_models), IndR_his_u_slope.sel(models=pcorr_models), input_core_dims="models")
+    IndR_NC_intermodels_rvalue_his_u_slope,
+    IndR_NC_intermodels_rvalue_his_u_intercept,
+    IndR_NC_intermodels_rvalue_his_u_rvalue,
+    IndR_NC_intermodels_rvalue_his_u_pvalue,
+    IndR_NC_intermodels_rvalue_his_u_hypothesis,
+) = ca.dim_linregress(IndR_his_NC_regress[2].sel(models=pcorr_models), IndR_his_u_rvalue.sel(models=pcorr_models), input_core_dims="models")
 
 (
-    IndR_NC_intermodels_slope_his_v_slope,
-    IndR_NC_intermodels_slope_his_v_intercept,
-    IndR_NC_intermodels_slope_his_v_rvalue,
-    IndR_NC_intermodels_slope_his_v_pvalue,
-    IndR_NC_intermodels_slope_his_v_hypothesis,
-) = ca.dim_linregress(IndR_his_NC_regress[2].sel(models=pcorr_models), IndR_his_v_slope.sel(models=pcorr_models), input_core_dims="models")
+    IndR_NC_intermodels_rvalue_his_v_slope,
+    IndR_NC_intermodels_rvalue_his_v_intercept,
+    IndR_NC_intermodels_rvalue_his_v_rvalue,
+    IndR_NC_intermodels_rvalue_his_v_pvalue,
+    IndR_NC_intermodels_rvalue_his_v_hypothesis,
+) = ca.dim_linregress(IndR_his_NC_regress[2].sel(models=pcorr_models), IndR_his_v_rvalue.sel(models=pcorr_models), input_core_dims="models")
+
+IndR_NC_intermodels_rvalue_his_wind_mask = ca.wind_check(
+    xr.where(IndR_NC_intermodels_rvalue_his_u_pvalue <= 0.05, 1.0, 0.0),
+    xr.where(IndR_NC_intermodels_rvalue_his_v_pvalue <= 0.05, 1.0, 0.0),
+    xr.where(IndR_NC_intermodels_rvalue_his_u_pvalue <= 0.05, 1.0, 0.0),
+    xr.where(IndR_NC_intermodels_rvalue_his_v_pvalue <= 0.05, 1.0, 0.0),
+)
+# %%
+#   plot the intermodels regression results
+startlevel=[-1.0, -1.0, -1.0]
+spacinglevel=[0.1, 0.1, 0.1]
+scalelevel=[0.17, 0.17, 0.17]
+pplt.rc.grid = False
+pplt.rc.reso = "lo"
+cl = 0  # 设置地图投影的中心纬度
+proj = pplt.PlateCarree(central_longitude=cl)
+
+fig = pplt.figure(span=False, share=False, refwidth=4.0, wspace=4.0, hspace=3.5, outerpad=2.0)
+plot_array = np.reshape(range(1, 4), (3, 1))
+# plot_array[-1,-3:] = 0
+axs = fig.subplots(plot_array, proj=proj)
+
+#   set the geo_ticks and map projection to the plots
+# xticks = np.array([30, 60, 90, 120, 150, 180])  # 设置纬度刻度
+xticks = np.array([30, 60, 90, 120, 150, 180])  # 设置纬度刻度
+yticks = np.arange(-30, 46, 15)  # 设置经度刻度
+# 设置绘图的经纬度范围extents，其中前两个参数为经度的最小值和最大值，后两个数为纬度的最小值和最大值
+# 当想要显示的经纬度范围不是正好等于刻度显示范围时，对extents进行相应的修改即可
+extents = [xticks[0], xticks[-1], yticks[0], 55.0]
+sepl.geo_ticks(axs, xticks, yticks, cl, 5, 5, extents)
+# ===================================================
+ski = 2
+n = 1
+w, h = 0.12, 0.14
+# ======================================
+for ax in axs:
+    rect = Rectangle((1 - w, 0), w, h, transform=ax.transAxes, fc="white", ec="k", lw=0.5, zorder=1.1)
+    ax.add_patch(rect)
+    # India area
+    x0 = India_W
+    y0 = India_S
+    width = India_E-India_W
+    height = India_N-India_S
+    patches(ax, x0 - cl, y0, width, height, proj)
+    # NC area
+    x0 = NC_W
+    y0 = NC_S
+    width = NC_E-NC_W
+    height = NC_N-NC_S
+    patches(ax, x0 - cl, y0, width, height, proj)
+    # #   IWF area
+    # x0 = 90
+    # y0 = 5.0
+    # width = 50.0
+    # height = 27.5
+    # patches(ax, x0 - cl, y0, width, height, proj)
+# ======================================
+for num_lev,lev in enumerate([200.0, 500.0, 850.0]):
+    con = axs[num_lev].contourf(
+        IndR_NC_intermodels_rvalue_his_hgt_rvalue.sel(level=lev),
+        cmap="ColdHot",
+        cmap_kw={"left": 0.06, "right": 0.94, "cut": -0.1},
+        levels=np.arange(startlevel[num_lev], -startlevel[num_lev]+spacinglevel[num_lev], spacinglevel[num_lev]),
+        zorder=0.8,
+    )
+    sepl.plt_sig(
+        IndR_NC_intermodels_rvalue_his_hgt_rvalue.sel(level=lev), axs[num_lev], n, np.where(IndR_NC_intermodels_rvalue_his_hgt_pvalue.sel(level=lev)[::n, ::n] <= 0.05), "bright purple", 3.0,
+    )
+    axs[num_lev].quiver(
+        IndR_NC_intermodels_rvalue_his_u_rvalue.sel(level=lev)[::ski, ::ski],
+        IndR_NC_intermodels_rvalue_his_v_rvalue.sel(level=lev)[::ski, ::ski],
+        zorder=1.1,
+        headwidth=2.6,
+        headlength=2.3,
+        headaxislength=2.3,
+        scale_units="xy",
+        scale=scalelevel[num_lev],
+        pivot="mid",
+        color="grey6",
+    )
+
+    m = axs[num_lev].quiver(
+        IndR_NC_intermodels_rvalue_his_u_rvalue.sel(level=lev).where(IndR_NC_intermodels_rvalue_his_wind_mask.sel(level=lev) > 0.0)[::ski, ::ski],
+        IndR_NC_intermodels_rvalue_his_v_rvalue.sel(level=lev).where(IndR_NC_intermodels_rvalue_his_wind_mask.sel(level=lev) > 0.0)[::ski, ::ski],
+        zorder=1.1,
+        headwidth=2.6,
+        headlength=2.3,
+        headaxislength=2.3,
+        scale_units="xy",
+        scale=scalelevel[num_lev],
+        pivot="mid",
+        color="black",
+    )
+
+    qk = axs[num_lev].quiverkey(
+        m, X=1 - w / 2, Y=0.7 * h, U=0.5, label="0.5", labelpos="S", labelsep=0.05, fontproperties={"size": 5}, zorder=3.1,
+    )
+    axs[num_lev].format(
+        rtitle="{:.0f}hPa".format(lev), ltitle="historical",
+    )
+    
+    # ======================================
+fig.colorbar(con, loc="b", width=0.13, length=0.7, label="")
+fig.format(abc="(a)", abcloc="l", suptitle="(hgt&U reg IndR) reg corr(IndR,NCR)")
 # %%
