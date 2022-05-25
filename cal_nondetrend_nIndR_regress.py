7c2d4816c4b39aec90c232f48a9eb0ed052c14fa
Hongjing0506 <18520228343@163.com>
2022-05-25 16:39:20 +0800

    create the file

---
+++ cal_nondetrend_nIndR_regress.py
@@ -0,0 +1,9 @@
+'''
+Author: ChenHJ
+Date: 2022-05-25 16:39:12
+LastEditors: ChenHJ
+LastEditTime: 2022-05-25 16:39:13
+FilePath: /chenhj/0302code/cal_nondetrend_nIndR_regress.py
+Aim: 
+Mission: 
+'''
