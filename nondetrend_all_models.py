2d741299c5dc8c66840e0179301d46b1e8b64cda
Hongjing0506 <18520228343@163.com>
2022-06-28 21:32:55 +0800

    create files

---
+++ nondetrend_all_models.py
@@ -0,0 +1,18 @@
+'''
+Author: ChenHJ
+Date: 2022-06-28 21:32:17
+LastEditors: ChenHJ
+LastEditTime: 2022-06-28 21:32:17
+FilePath: /chenhj/0302code/nondetrend_all_models.py
+Aim: 
+This code is to plot the detrended results of all models
+including:
+1) precipitation regress on SASMR
+2) circulation regress on SASMR at 200hPa, 500hPa and 850hPa
+3) sea surface temperature regress on SASMR
+4) WAhigh related to SASMR
+5) EAhigh related to SASMR
+6) WNPhigh related to SASMR
+Mission: 
+'''
+
