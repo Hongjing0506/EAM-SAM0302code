599bd3dbd8b9526eccff1ee6e7958d93daa3f510
Hongjing0506 <18520228343@163.com>
2022-06-28 16:59:55 +0800

    create the file

---
+++ detrend_good_models.py
@@ -0,0 +1,14 @@
+'''
+Author: ChenHJ
+Date: 2022-06-28 16:29:43
+LastEditors: ChenHJ
+LastEditTime: 2022-06-28 16:41:17
+FilePath: /chenhj/0302code/detrend_good_models.py
+Aim: 
+This code is to plot the detrended results of good models
+including:
+1) precipitation regress on SASMR
+2) circulation regress on SASMR at 200hPa, 500hPa and 850hPa
+3) sea surface temperature regress on SASMR
+Mission: 
+'''
