01f31702e6e789ea5ca67e969a1751af42546ebe
Hongjing0506 <18520228343@163.com>
2022-09-11 14:23:54 +0800

    create a file to calculate the standadize Indian precipitation

---
+++ final_figures_stand.py
@@ -0,0 +1,19 @@
+'''
+Author: ChenHJ
+Date: 2022-09-11 13:52:35
+LastEditors: ChenHJ
+LastEditTime: 2022-09-11 14:11:22
+FilePath: /chenhj/0302code/final_figures_stand.py
+Aim: This file is to plot final figures in paper.
+There are 8 figures in paper.
+recalculate the India precipitation here
+1) precipitation regress onto SASMR, observation and MME, 1979-2014
+2) circulation regress onto SASMR, observation and MME, 1979-2014, 200/500/850hPa
+3) taylor-diagram including 200hPa pcc and 850hPa pcc
+4) precipitation regress onto SASMR, gMME, 1979-2014 & 2064-2099 & diff
+5) circulation regress onto SASMR, gMME, 1979-2014 & 2064-2099 & diff, 200/500/850hPa
+6) divergence at 200 hPa and 850 hPa & omega at 500 hPa, all of them regress onto SASMR, gMME for the periods of 1979-2014 and 2064-2099
+7) bar plots that show the WAAC, EAAC and WNPAC in all 26 models, gMME and MME in the period of 1979-2014 & 2064-2099 & diff
+8) Sea surface temperature regress onto SASMR, observation and gMME, 1979-2014 & 2064-2099 & diff
+Mission: 
+'''
